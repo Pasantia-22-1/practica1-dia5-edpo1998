fb2e7cca918648b269f41429d7a1053d0e126262
edpo1998 <edpo11081998@gmail.com>
2022-03-18 10:31:50 -0600

    fase1

---
+++ js/app.js
@@ -0,0 +1,28 @@
+"use strict"
+console.log("Aplicacion Calculadora");
+
+function restar(){
+    const forma = document.getElementById("forma");
+    let operandoA = forma["operandoA"];
+    let operandoB = forma["operandoB"];
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = "La operacion no incluye numeros";
+    document.getElementById("resultadoR").innerHTML = `Resultado ${resultado}`;
+    console.log(`Resultado ${resultado}`) ;
+}
+
+function sumar(){
+    const forma = document.getElementById("forma");
+    let operandoA = forma["operandoA"];
+    let operandoB = forma["operandoB"];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = "La operacion no incluye numeros";
+    document.getElementById("resultado").innerHTML = `Resultado ${resultado}`;
+    console.log(`Resultado ${resultado}`) ;
+}
+
+function clean(){
+    location.reload();
+}
